bf97afee91a0b1ff31036c770d4316d5fad93e17
INOUE Takuya <inouetakuya5@gmail.com>
2018-02-18 22:50:33 +0900

    ESLint として airbnb-base を使うようにした

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,8 @@ module.exports = {
     parser: 'babel-eslint'
   },
   extends: [
+    'airbnb-base',
+
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     'plugin:vue/essential'
