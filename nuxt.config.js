ce541ea5fe7ef346d7a51a9df83b533bd51d2579
HANATANI Takuma <mail@potato4d.me>
2018-03-07 21:01:27 +0900

    メタタグの追加 (#33)
    * Add SEO meta and OGP
    
    * Add twitter:creator
    
    * Replace double quote to single quote

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,14 +5,28 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'vuefes-2018',
+    title: 'Vue Fes Japan 2018 | 2018年秋開催予定',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Vue Fes Japan 2018' },
+      { hid: 'description', name: 'description', content: '日本で初めて開催する大規模Vue.jsカンファレンス。国内外の著名スピーカーによるセッションの他、ユーザー同士が気軽に話し合える場も設ける予定です。ぜひ、一緒にVue.jsを楽しみ、盛り上げていきましょう！' },
+      { name: 'og:locale', content: 'ja_JP' },
+      { name: 'og:type', content: 'website' },
+      { name: 'og:title', content: 'Vue Fes Japan 2018' },
+      { name: 'og:description', content: '日本で初めて開催する大規模Vue.jsカンファレンス。国内外の著名スピーカーによるセッションの他、ユーザー同士が気軽に話し合える場も設ける予定です。ぜひ、一緒にVue.jsを楽しみ、盛り上げていきましょう！' },
+      { name: 'og:url', content: 'https://vuefes.jp' },
+      { name: 'og:site_name', content: 'Vue Fes Japan 2018' },
+      { name: 'og:image', content: 'https://vuefes.jp/opengraph.png' },
+      { name: 'og:image:secure_url', content: 'https://vuefes.jp/opengraph.png' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:description', content: '日本で初めて開催する大規模Vue.jsカンファレンス。国内外の著名スピーカーによるセッションの他、ユーザー同士が気軽に話し合える場も設ける予定です。ぜひ、一緒にVue.jsを楽しみ、盛り上げていきましょう！' },
+      { name: 'twitter:creator', content: '@vuefes' },
+      { name: 'twitter:title', content: 'Vue Fes Japan 2018' },
+      { name: 'twitter:image', content: 'https://vuefes.jp/opengraph.png' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', href: '/apple-touch-icon.png', sizes: '180x180' }
     ],
   },
   /*
