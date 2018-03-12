30032a980f1b00a578ae8d2abe0161f81936a7e0
INOUE Takuya <inouetakuya5@gmail.com>
2018-03-12 22:46:56 +0900

    Vue Fes Japan 2018 実行委員会メンバーの画像を表示させる (#34)
    * wip
    
    * スタッフの画像を追加した
    
    * スタッフのアイコンと Twitter ID を表示させるようにした
    
    * Twitter ページへのリンクを設定した
    
    * スタッフの画像を 3列に並べた
    
    * 余白を調整した
    
    * + text-decoration: none;
    
    * Vue.js 日本ユーザーグループへのリンクを設置した
    
    * 画像を JPEG mini で圧縮した
    
    * 画像を TinyPNG で圧縮した
    
    * 余白の定義を厳密に行うようにした
    
    * target="_blank" のときには rel="noopener" を付けるようにした
    
    https://github.com/kazupon/vuefes-2018/pull/34#discussion_r173679994
    
    * readOnly なデータをキャッシュさせたいので computed に移した
    
    * ImageOptim でさらに圧縮できた

--- src/components/TeamSection.vue
+++ src/components/TeamSection.vue
@@ -3,12 +3,21 @@
     <template slot="heading">
       About us
     </template>
-    <div>
-      <p>Vue Fes Japan 2018 は、Vue Fes Japan 2018 実行委員会によって企画・運営されています。同会は、代表と有志のスタッフで組織されており、その全員が無償で協力しています。人件費、交通費など、あらゆる名目での対価を受け取っていません。</p>
+
+    <div class="description">
+      <p class="readable">Vue Fes Japan 2018 は、<a href="https://github.com/vuejs-jp/home" target="_blank">Vue.js 日本ユーザーグループ</a> のスタッフが中心となって結成された、Vue Fes Japan 2018 実行委員会によって企画・運営されています。同会は、代表と有志のスタッフで組織されており、その全員が無償で協力しています。人件費、交通費など、あらゆる名目での対価を受け取っていません。</p>
     </div>
-    <div>
-      TODO: スタッフの画像
-    </div>
+
+    <ul class="staffs">
+      <li class="staff" v-for="staff in staffs" :key="staff.twitterId">
+        <a :href="`https://twitter.com/${staff.twitterId}`" target="_blank" rel="noopener">
+          <img class="icon" :src="require(`~/assets/images/staffs/${staff.icon}`)">
+        </a>
+        <a class="link-to-twitter" :href="`https://twitter.com/${staff.twitterId}`" target="_blank" rel="noopener">
+          {{ staff.twitterId }}
+        </a>
+      </li>
+    </ul>
   </page-section>
 </template>
 
@@ -20,9 +29,61 @@ export default {
   components: {
     PageSection,
   },
+  computed: {
+    staffs () {
+      return [
+        { twitterId: '@448jp', icon: '448jp.png' },
+        { twitterId: '@kazu_pon', icon: 'kazu_pon.png' },
+        { twitterId: '@kawakami0717', icon: 'kawakami0717.jpg' },
+        { twitterId: '@kazuyukimiyake', icon: 'kazuyukimiyake.jpg' },
+        { twitterId: '@masaakikunsan', icon: 'masaakikunsan.jpg' },
+        { twitterId: '@inouetakuya', icon: 'inouetakuya.jpg' },
+        { twitterId: '@c5meru', icon: 'c5meru.jpg' },
+        { twitterId: '@hashedrock', icon: 'hashedrock.jpg' },
+        { twitterId: '@shika358', icon: 'shika358.jpg' },
+        { twitterId: '@potato4d', icon: 'potato4d.jpg' },
+        { twitterId: '@positiveflat', icon: 'positiveflat.png' },
+        { twitterId: '@hypermkt', icon: 'hypermkt.png' },
+      ]
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
-// TODO: Not yet implemented
+.team-section {
+  padding-bottom: 40px;
+}
+
+.description {
+  margin-bottom: 20px;
+}
+
+.staffs {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.staff {
+  width: calc((100% - (6px * 2)) / 3);
+  margin-top: 15px;
+}
+
+.staff:nth-of-type(3n-1),
+.staff:nth-of-type(3n) {
+  margin-left: 6px;
+}
+
+.icon {
+  display: block;
+  margin-bottom: 4px;
+}
+
+.link-to-twitter {
+  display: block;
+  text-align: center;
+  font-size: 12px;
+  color: $black;
+  text-decoration: none;
+}
 </style>
