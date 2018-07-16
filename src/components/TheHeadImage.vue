409bc7abfb1c577ab3e115338643167cfeeb9cb2
INOUE Takuya <inouetakuya5@gmail.com>
2018-07-17 01:39:00 +0900

    ヘッド画像を圧縮する (#178)
    * ヘッド画像を圧縮した
    
    * ファイル名を実態を表すように変更した
    
    * 画像サイズを小さくして ImageOptim で圧縮したヘッド画像を作成した
    
    * ピクセル密度デスクリプタを使って画像を出し分けるようにした
    
    * 現在 4x の画像は使用していないため削除した
    
    * Revert "現在 4x の画像は使用していないため削除した"
    
    This reverts commit bb5cae763d7d62112abfdecdc12539ba6ad3d1af.
    
    * Galaxy Note 8 のピクセル密度（dppx）が 4x だった

--- src/components/TheHeadImage.vue
+++ src/components/TheHeadImage.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="the-head-image">
     <picture>
-      <source media="(max-width: 768px)" srcset="~/assets/images/head@2x.jpg">
+      <source
+        media="(max-width: 768px)"
+        srcset="~/assets/images/head@2x.jpg,
+          ~/assets/images/head@2x.jpg 2x,
+          ~/assets/images/head@3x.jpg 3x,
+          ~/assets/images/head@4x.jpg 4x"
+      >
       <source media="(min-width: 769px)" srcset="~/assets/images/head.jpg">
       <img class="head-image" src="~/assets/images/head.jpg" alt="Vue Fes Japan 2018 | 2018.11.3 [Sat.] | 秋葉原 UDX 4F UDX ギャラリー">
     </picture>
